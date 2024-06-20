f16cadb6d433cb9ef846722bd37dcad87ebe3d07
glnb <80047445+glnb@users.noreply.github.com>
2024-06-20 08:40:16 +0200

    Template & Views & Urls

--- Library/catalog/views.py
+++ Library/catalog/views.py
@@ -1,3 +1,20 @@
+import datetime
+
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from datetime import datetime
+def index(request):
+    date_jour = datetime.now()
+    #html = "<html><body>Il est %s.</body></html>" % date_jour
+    #html = f'<html><body>Il est {date_jour}.</body></html>'
+    #return HttpResponse(html)
+    VAR={
+            'date':date_jour,
+            'pseudo': 'Tom Tom'
+        }
+    return render(
+        request,
+        'catalog/index.html',
+        context=VAR
+    )
