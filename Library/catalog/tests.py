bb52724d718bdd96f90e3f24b1a61a784840867e
glnb <80047445+glnb@users.noreply.github.com>
2024-06-21 16:23:50 +0200

    Author

--- Library/catalog/tests.py
+++ Library/catalog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import *
+from datetime import datetime
 
 class GenreModelTest(TestCase):
     def test_string_representation(self):
@@ -18,3 +19,29 @@ class GenreModelTest(TestCase):
 
 
 class AuthorModelTest(TestCase):
+    def test_string_representation(self):
+        test_author = Author(name="Pablo")
+        self.assertEqual(str(test_author), test_author.name)
+        # str(genre) == genre.__str__()
+
+    def test_author_creation(self):
+        test_author = Author.objects.create(
+            name="Pablo",
+            date_birth="2024-06-23",
+            date_death="2200-06-26")
+        self.assertTrue(isinstance(test_author, Author))
+        # Ne fonctione pas encore
+        # self.assertLess(
+        #     self,
+        #     (datetime.strptime(test_author.date_birth, "%Y-%m-%d").timestamp()),
+        #     (datetime.strptime(test_author.date_death, "%Y-%m-%d").timestamp()))
+        self.assertEqual(test_author.__str__(), test_author.name)
+
+class BookModelTest(TestCase):
+    def
+
+
+
+
+
+
